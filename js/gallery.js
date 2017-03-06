6e11b53a1a784f9da0cc481de9a8944450888dfe
David Lall <david.lall718@gmail.com>
2017-03-06 10:19:43 -0500

    Moved javascript, added menu to gallery page.

---
+++ js/gallery.js
@@ -0,0 +1,12 @@
+var cpo = [];
+cpo["_object"] ="cp_widget_c5a3ab0a-7fc9-4fef-ad29-9b1c4fdf871a";
+cpo["_fid"] = "AANAh6tgHS5O";
+var _cpmp = _cpmp || [];
+_cpmp.push(cpo);
+(function() {
+var cp = document.createElement("script");
+cp.type = "text/javascript";
+cp.async = true;
+cp.src = "//www.cincopa.com/media-platform/runtime/libasync.js";
+var c = document.getElementsByTagName("script")[0];
+c.parentNode.insertBefore(cp, c); })();
